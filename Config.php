ff26060f4df334c171b1f062b6d5c48845b6d64d
SamLin <sam_lin@efatek.com>
2018-12-13 17:38:51 +0800

    放上config

---
+++ Config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sam
+ * Date: 2018/11/21
+ * Time: 下午 03:20
+ */
+
+class Config {
+	public $mysql_address = ""; // ---> 資料庫位置
+	public $mysql_username = ""; // ---> 資料庫帳號
+	public $mysql_password = ""; // ---> 資料庫密碼
+	public $mysql_database = ""; // ---> 資料庫名稱
+}
